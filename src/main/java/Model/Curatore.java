edb57337c827563d6d950cd3524fc1665713b545
Cristina Di Girolami <sacrvacri@gmail.com>
2025-11-26 10:23:38 +0100

    curatore modificato

--- src/main/java/Model/Curatore.java
+++ src/main/java/Model/Curatore.java
@@ -1,15 +1,10 @@
-package Model;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "curatori")
-public class Curatore {
+public class Curatore extends AbstractUtente{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,16 +13,19 @@ public class Curatore {
     @Column(nullable = false)
     private String nome;
 
-    @OneToMany(mappedBy = "curatoreValidatore", fetch = FetchType.LAZY)
-    private Set<Certificazione> certificazioni = new HashSet<>();
-
     public Curatore() {}
-    public Curatore(Long id, String nome) { this.id = id; this.nome = nome; }
+    public Curatore(Long id, String nome, String email, String password) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+        this.password = password;
+        this.ruolo = "ROLE_CURATORE";
+        this.approvato = false; // Serve approvazione
+    }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public Set<Certificazione> getCertificazioni() { return certificazioni; }
-    public void setCertificazioni(Set<Certificazione> certificazioni) { this.certificazioni = certificazioni;}
+    public void setNome(String nome) { this.nome=nome;}
 }
