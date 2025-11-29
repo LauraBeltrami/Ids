3a7f8f4abdf0de9566f099132fc917e6a1e1b46f
Laura Beltrami <laura.beltrami@studenti.unicam.it>
2025-11-29 11:59:52 +0100

    Cerificazione DTO

--- src/main/java/DTO/CertificazioneDTO.java
+++ src/main/java/DTO/CertificazioneDTO.java
@@ -2,10 +2,9 @@ package DTO;
 
 import java.time.LocalDateTime;
 
+
 public record CertificazioneDTO(
         Long id,
-        String descrizione,
-        Long curatoreId,
-        String curatoreNome,
-        LocalDateTime dataApprovazione
-){}
+        String descrizione
+
+) {}
